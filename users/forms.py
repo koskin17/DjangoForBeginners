7384174a0b22eb574df47d01603f09a00bc4e746
koskin <kostia.zeykin@gmail>
2025-07-07 16:15:37 +0300

    add forms for users and add help about class Meta

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import AuthenticationForm
+
+from users.models import User
+
+class UserLoginForm(AuthenticationForm):
+    """
+    Кастомная форма логина, которая наследуюется от Django's AuthenticationForm.
+    Она может быть расширена дополнительными полями или методами валидации.
+    """
+    class Meta:
+        model = User
+        fields = ['username', 'password']   # Подробнее про class Meta: file:///E:/My%20project/DjangoForBeginners/Help/Class%20Meta.txt
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+# Импортируем контроллеры
+from users.views import login, register
+
+# Обязательно создаётся переменная app_name с названием нашего приложения 'products'. Это позволяет django понимать к какому приложению относятся ниже приведённые маршруты
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('register/', register, name='register')
+]
