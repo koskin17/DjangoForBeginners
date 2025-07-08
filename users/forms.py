b993e9e1f7748c2d8f274676ad75a4f968562dc9
koskin <kostia.zeykin@gmail>
2025-07-08 13:03:29 +0300

    completed a lesson on custom templates and form customization

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django import forms # Этот файл импортируется для кастомизации форм
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
@@ -7,6 +8,19 @@ class UserLoginForm(AuthenticationForm):
     Кастомная форма логина, которая наследуется от Django's AuthenticationForm.
     Она может быть расширена дополнительными полями или методами валидации.
     """
+    
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите имя пользователя'})) # Задаётся строковый тип данных и указывается виджет о том, что происходит в этой форме. В atts в виде словаря указываются все дополнительные параметры из html-верстки
+    
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Введите пароль'}))
+    
     class Meta:
         model = User
         fields = ['username', 'password']   # Подробнее про class Meta: file:///E:/My%20project/DjangoForBeginners/Help/Class%20Meta.txt
+        
+        
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'            
+        
